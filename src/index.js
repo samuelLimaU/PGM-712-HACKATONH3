f2b06390fda58c57176aa7fc12ac83f2d9024a0a
Adriana Ayllon <ayllon814@gmail.com>
2026-04-29 21:07:10 -0400

    avance parte grafica

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './RetroAesthetic.css'; // New standalone styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div id="retro-aesthetic-overlay"></div>
     <App />
   </React.StrictMode>
 );
